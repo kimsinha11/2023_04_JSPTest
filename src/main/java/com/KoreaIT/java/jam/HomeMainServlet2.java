3b92c979760036d2c0edc13b44ceb843a7cd4952
kimsinha11 <tlsgk2013@gmail.com>
2023-04-06 14:57:29 +0900

    구구단 파라미터 사용

--- src/main/java/com/KoreaIT/java/jam/HomeMainServlet2.java
+++ src/main/java/com/KoreaIT/java/jam/HomeMainServlet2.java
@@ -15,12 +15,28 @@ public class HomeMainServlet2 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
-
-		int dan = 8;
+		
+//inputedDan은 /printDan? 물음표 뒤에 입력된 값(파라미터)을 받아온다
+//파라미터를 여러개 받고싶으면 &로 엮어주면 된다. printDan?dan=9&limit=3
+		String inputedDan = request.getParameter("dan");
+		String inputedLimit = request.getParameter("limit");
+		
+		if (inputedDan == null) {
+			inputedDan = "1";
+		}
+		
+		if (inputedLimit == null) {
+			inputedLimit = "1";
+		}
+		
+		int dan = Integer.parseInt(inputedDan);
+		int limit = Integer.parseInt(inputedLimit);
+		
 		response.getWriter().append(dan + "단" + "<br>");
 
-		for (int j = 1; j < 10; j++) {
+		for (int j = 1; j <= limit; j++) {
 			response.getWriter().append(dan + "*" + j + " = " + dan * j + "<br>");
+//			response.getWriter().append(String.format("%d * %d = %d<br>",dan,i);
 		}
 
 	}
