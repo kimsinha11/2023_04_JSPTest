5445d224a7525e6a2a4b787153ad4e62f75fba10
kimsinha11 <tlsgk2013@gmail.com>
2023-04-06 14:38:07 +0900

    구구단 기초, 파라미터 X

--- src/main/java/com/KoreaIT/java/jam/HomeMainServlet2.java
+++ src/main/java/com/KoreaIT/java/jam/HomeMainServlet2.java
@@ -1,20 +1,28 @@
 package com.KoreaIT.java.jam;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/home/main2") //http://localhost:8081/2023_JSP_AM/home/main2
+@WebServlet("/printDan") // http://localhost:8081/2023_JSP_AM/home/main2
 public class HomeMainServlet2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-   
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		response.getWriter().append("dongyeon pig").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+
+		int dan = 8;
+		response.getWriter().append(dan + "단" + "<br>");
+
+		for (int j = 1; j < 10; j++) {
+			response.getWriter().append(dan + "*" + j + " = " + dan * j + "<br>");
+		}
+
 	}
 
-	
 }
