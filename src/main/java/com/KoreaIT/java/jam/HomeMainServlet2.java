bca4ee95054fc7091cbee76c4a823035c70a9fce
kimsinha11 <tlsgk2013@gmail.com>
2023-04-06 15:27:06 +0900

    파라미터에 따라서 텍스트 색상 변경

--- src/main/java/com/KoreaIT/java/jam/HomeMainServlet2.java
+++ src/main/java/com/KoreaIT/java/jam/HomeMainServlet2.java
@@ -20,7 +20,8 @@ public class HomeMainServlet2 extends HttpServlet {
 //파라미터를 여러개 받고싶으면 &로 엮어주면 된다. printDan?dan=9&limit=3
 		String inputedDan = request.getParameter("dan");
 		String inputedLimit = request.getParameter("limit");
-		
+		String inputedColor = request.getParameter("color");
+
 		if (inputedDan == null) {
 			inputedDan = "1";
 		}
@@ -29,14 +30,15 @@ public class HomeMainServlet2 extends HttpServlet {
 			inputedLimit = "1";
 		}
 		
+
 		int dan = Integer.parseInt(inputedDan);
 		int limit = Integer.parseInt(inputedLimit);
-		
-		response.getWriter().append(dan + "단" + "<br>");
+		String color = inputedColor;
+		response.getWriter().append(String.format("<div style=\"color:%s;\">==%d단==</div>",color,dan));
 
 		for (int j = 1; j <= limit; j++) {
-			response.getWriter().append(dan + "*" + j + " = " + dan * j + "<br>");
-//			response.getWriter().append(String.format("%d * %d = %d<br>",dan,i);
+//			response.getWriter().append(dan + "*" + j + " = " + dan * j + "<br>");
+			response.getWriter().append(String.format("<div style=\"color:%s;\">%d * %d = %d<br></div>",color, dan,j,dan*j));
 		}
 
 	}
