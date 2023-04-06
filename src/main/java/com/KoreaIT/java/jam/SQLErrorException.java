823007d0fbaae88f302b456eaa9520b89e95d408
kimsinha11 <tlsgk2013@gmail.com>
2023-04-06 17:19:05 +0900

    DB 연결해서 데이터 가져오기

---
+++ src/main/java/com/KoreaIT/java/jam/SQLErrorException.java
@@ -0,0 +1,14 @@
+package com.KoreaIT.java.jam;
+
+public class SQLErrorException extends RuntimeException {
+	private Exception origin;
+
+	public SQLErrorException(String message, Exception origin) {
+		super(message);
+		this.origin = origin;
+	}
+
+	public Exception getOrigin() {
+		return origin;
+	}
+}
